879d8ae2160aff45c0547e884a657676811f1083
kgridou <32600911+kgridou@users.noreply.github.com>
2026-02-13 09:19:41 +0100

    wip

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ScButton } from '@semantic-components/ui';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ScButton],
   template: `
     <h1>Hello, {{ title() }}</h1>
+
+    <button sc-button>Default</button>
 
     <router-outlet />
   `,
